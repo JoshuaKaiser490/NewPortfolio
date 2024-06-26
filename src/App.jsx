c8104b575926a7f3e377477b5b9f89ed55e1a811
Joshua Kaiser <jmk@Mini-von-Joshua.fritz.box>
2024-06-26 20:16:13 +0200

    About section Grundgerüst fertig

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import styles from "./App.module.css";
 import About from "./components/About/About";
 import  Hero  from "./components/Hero/Hero";
-import  Navbar  from "./components/Navbar/Navbar";
+import { Navbar } from "./components/Navbar/Navbar";
 
 
 function App() {
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 
+import styles from "./About.module.css";
+import { getImageUrl } from '../../utils'
+
 function About() {
   return (
     <div>
-      About
+      <section>
+        <h2>About</h2>
+        <div>
+          <img src={getImageUrl("about/aboutImage.png")} alt="Me on the Mac" />
+          <ul>
+            <li>
+              <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
+              <div>
+               <h3>Frontend Developer</h3>
+               <p>
+                I´m a frontend developer with expirence in building responsive an optimized sites
+               </p>
+              </div>
+            </li>
+            <li>
+              <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
+              <div>
+               <h3>Backend Developer</h3>
+               <p>
+                I have experience developing fast and optimised back-end systems
+                and APIs
+              </p>
+            </div>
+          </li>
+          <li>
+              <img src={getImageUrl("about/uiIcon.png")} alt="UI Icon" />
+              <div>
+               <h3>UI Designer</h3>
+               <p>
+               I have designed multiple landing pages and have created design
+                systems as well
+              </p>
+            </div>
+          </li>
+          </ul>
+          </div>
+      </section>
     </div>
   )
 }
