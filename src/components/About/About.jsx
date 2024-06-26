235e476992f34cc661b40b9f21a515483acf46b8
Joshua Kaiser <jmk@Mini-von-Joshua.fritz.box>
2024-06-26 20:38:50 +0200

    About section gestyled und das Scroll verhalten auf smooth gesetzt

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
-
 import styles from "./About.module.css";
 import { getImageUrl } from '../../utils'
 
 function About() {
   return (
-    <div>
-      <section>
-        <h2>About</h2>
-        <div>
-          <img src={getImageUrl("about/aboutImage.png")} alt="Me on the Mac" />
-          <ul>
-            <li>
+      <section className={styles.container} id='about'>
+        <h2 className={styles.title}>About</h2>
+        <div className={styles.content}>
+          <img 
+          src={getImageUrl("about/aboutImage.png")} 
+          alt="Me on the Mac"
+          className={styles.aboutImage} />
+          <ul className={styles.aboutItems}>
+            <li className={styles.aboutItem}>
               <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
-              <div>
+              <div className={styles.aboutItemText}>
                <h3>Frontend Developer</h3>
                <p>
                 I´m a frontend developer with expirence in building responsive an optimized sites
                </p>
               </div>
             </li>
-            <li>
+            <li className={styles.aboutItem} >
               <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
-              <div>
+              <div className={styles.aboutItemText}>
                <h3>Backend Developer</h3>
                <p>
                 I have experience developing fast and optimised back-end systems
@@ -30,9 +31,9 @@ function About() {
               </p>
             </div>
           </li>
-          <li>
+          <li className={styles.aboutItem}>
               <img src={getImageUrl("about/uiIcon.png")} alt="UI Icon" />
-              <div>
+              <div className={styles.aboutItemText}>
                <h3>UI Designer</h3>
                <p>
                I have designed multiple landing pages and have created design
@@ -43,7 +44,7 @@ function About() {
           </ul>
           </div>
       </section>
-    </div>
+ 
   )
 }
 
