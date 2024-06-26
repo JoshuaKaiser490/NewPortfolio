26cc591f4144f0ca16640032cc785ace088892bf
Joshua Kaiser <jmk@Mini-von-Joshua.fritz.box>
2024-06-26 15:49:39 +0200

    Hero section angefangen

---
+++ src/components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { getImageUrl } from '../../../utils'
+import styles from "./Hero.module.css"
+
+const Hero = () => {
+  return( 
+  <section className={styles.container}>
+    <div className={styles.content}>
+        
+        <h1 className={styles.title}>Hi, i´m Joshua</h1>
+        <p className={styles.description}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum cumque temporibus voluptatum cum odio sed vero veritatis eos! Libero, quibusdam. Sa?</p>
+        <a href="mailto:joshua-marcel.kaiser@web.de" className={styles.contactBtn}>Contact me</a>
+    
+    </div>
+    <img  src={getImageUrl("hero/heroImage.png")} alt="Hero Image of me" className={styles.heroImg}/>
+    <div className={styles.topBlur}></div>
+    <div className={styles.bottomBlur}></div>
+  </section>
+  );
+};
+
+export default Hero
