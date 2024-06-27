47d375876d13fa026b6f8ce79bc5d4accb1a3223
Joshua Kaiser <jmk@Mini-von-Joshua.fritz.box>
2024-06-27 12:34:04 +0200

    Projekte angefangen zu stylen

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import projects from "../../data/projects.json"
+import React from "react";
 
-import { ProjectCard } from './ProjectCard'
+import styles from "./Projects.module.css";
+
+import projects from "../../data/projects.json";
+import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
-  return ( 
-  <section>
-    <h2>Projects</h2>
-    <div>
-        {projects.map((project, id) => {
-            return (
-            <ProjectCard key={id} project={project}/>
-            )
-        })}
-    </div>
-  </section>
-  )
-}
+    return (
+      <section className={styles.container} id="projects">
+        <h2 className={styles.title}>Projects</h2>
+        <div className={styles.projects}>
+          {projects.map((project, id) => {
+            return <ProjectCard key={id} project={project} />;
+          })}
+        </div>
+      </section>
+    );
+  };
 
 export default Projects
