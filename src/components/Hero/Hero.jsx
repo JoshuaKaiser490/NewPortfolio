c07a327c1f27eb94ab9a4b0741ccd88f0a025b79
Joshua Kaiser <jmk@Mini-von-Joshua.fritz.box>
2024-06-29 16:15:01 +0200

    Typewriter effect eingebaut, und responsivenes angepasst.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,24 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { getImageUrl } from '../../utils'
 import styles from "./Hero.module.css"
-import Typewriter from 'typewriter-effect';
+import { Typewriter } from 'react-simple-typewriter';
 
 const Hero = () => {
   return( 
   <section className={styles.container}>
     <div className={styles.content}>
         
-        <h1 className={styles.title}>Hi, I´m Joshua</h1>
+        <h1 className={styles.title}>
+          <Typewriter
+            words={["Hi, I'm Joshua.", "Web developer from Germany.", "Welcome to my portfolio!"]}
+            loop={1}
+            cursor
+            cursorStyle='|'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </h1>
         <p className={styles.description}>I'm Joshua, a 26-year-old web developer from Germany. With solid experience in web development, I work with HTML, CSS, JavaScript, and React for the frontend, and Node.js for the backend. I'm excited to keep exploring and growing in this field 🖥️.</p>
         <a href="mailto:joshua-marcel.kaiser@web.de" className={styles.contactBtn}>Contact me</a>
     
@@ -20,4 +30,4 @@ const Hero = () => {
   );
 };
 
-export default Hero
+export default Hero;
